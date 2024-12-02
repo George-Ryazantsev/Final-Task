d025653ee801cc591c9742589c9bce17c353bdfd
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-12-02 22:04:34 +0300

    Some lines removed

--- Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
+++ Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
@@ -9,16 +9,14 @@
     {
         [Test]
         public async Task UpdateFileAsync_ShouldDownloadAndUnzipFile()
-        {
-            // Arrange
+        {            
             string testZipContentPath = "test.zip";
             string extractedFileName = "test.txt";
             string extractedContent = "Hello, NUnit!";
             string destinationPath = "Destination/";
 
             Directory.CreateDirectory(destinationPath);
-
-            // Создаем тестовый ZIP-файл
+            
             using (var zipStream = new FileStream(testZipContentPath, FileMode.Create))
             using (var archive = new ZipArchive(zipStream, ZipArchiveMode.Create))
             {
@@ -36,17 +34,14 @@
 
             var httpClient = new HttpClient(fakeHttpHandler);
             var service = new FileUpdateService(httpClient);
-
-            // Act
+            
             var fileName = await service.UpdateFileAsync("https://example.com/test.zip", destinationPath);
-
-            // Assert
+            
             string unzippedFilePath = Path.Combine("UnZip files", extractedFileName);
             Assert.That(File.Exists(Path.Combine(destinationPath, fileName)), Is.True, "Downloaded file does not exist.");
             Assert.That(File.Exists(unzippedFilePath), Is.True, "Unzipped file does not exist.");
             Assert.That(await File.ReadAllTextAsync(unzippedFilePath), Is.EqualTo(extractedContent), "Unzipped file content does not match.");
-
-            // Cleanup
+            
             Directory.Delete(destinationPath, true);
             Directory.Delete("UnZip files", true);
             File.Delete(testZipContentPath);
@@ -54,13 +49,11 @@
 
         [Test]
         public async Task UpdateFileAsync_ShouldHandleNonExistentFile()
-        {
-            // Arrange
+        {            
             var fakeHttpHandler = new FakeHttpMessageHandler(new Dictionary<string, byte[]>());
             var httpClient = new HttpClient(fakeHttpHandler);
             var service = new FileUpdateService(httpClient);
-
-            // Act & Assert
+            
             Assert.DoesNotThrowAsync(async () =>
             {
                 await service.UpdateFileAsync("https://example.com/missing.zip", "Destination/");
