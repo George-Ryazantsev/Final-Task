edd5abb27364d6320cec18d8ee355f6e827e63a9
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-12-03 23:22:05 +0300

    Check tests

--- Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
+++ Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
@@ -5,27 +5,7 @@
 
     [TestFixture]
     public class FileUpdateServiceTests
-    {
-        [Test]
-        public async Task UpdateFileAsync_ShouldDownloadAndUnzipFile()
-        {
-            string destinationPath = Path.Combine(Directory.GetCurrentDirectory(), "Archived files\\");
-            string unZipFilePath = Path.Combine(Directory.GetCurrentDirectory(), "UnZip files\\");
-
-            Directory.CreateDirectory(destinationPath);                                   
-            Directory.CreateDirectory(unZipFilePath);                                                       
-
-            var httpClient = new HttpClient();
-            var service = new FileUpdateService(httpClient);
-            
-            var fileName = await service.UpdateFileAsync("https://www.dropbox.com/scl/fi/pvx75x4vqlv6qzjlcu9r5/File-For-test.zip?rlkey=yphtkchtda7jz7gw6nuh2yw6f&st=ogyl5u7f&dl=1", destinationPath);                       
-
-            Assert.That(File.Exists(Path.Combine(destinationPath, fileName)), Is.True, "Downloaded file does not exist.");         
-            
-            Directory.Delete(destinationPath, true);
-            Directory.Delete(unZipFilePath, true);
-
-        }
+    {      
 
         [Test]
         public async Task UpdateFileAsync_ShouldHandleNonExistentFile()
