e058900b3fbcb7af7f1ee87bed25937edcbff3cd
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-12-03 15:22:14 +0300

    Changes in tests

--- Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
+++ Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
@@ -18,33 +18,7 @@
             {
                 await service.UpdateFileAsync("https://example.com/missing.zip", "Destination/");
             }, "Method threw an exception for a non-existent file.");
-        }
-
-       /* [Test]
-        public async Task UpdateFileAsync_ShouldHandleDownloadFile()
-        {
-            string fileUrl = "https://www.dropbox.com/scl/fi/pvx75x4vqlv6qzjlcu9r5/File-For-test.zip?rlkey=yphtkchtda7jz7gw6nuh2yw6f&st=6c72ojex&dl=1";
-
-            string destinationPath = Path.Combine(Directory.GetCurrentDirectory(), "TestDestination\\");
-            string unZipFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "UnZip files\\");          
-            string fakeDownloadedFileName = "File For test.zip";      
-
-            Directory.CreateDirectory(destinationPath);
-            Directory.CreateDirectory(unZipFolderPath);
-         
-
-            var httpClient = new HttpClient();
-            var service = new FileUpdateService(httpClient);
-                     
-            string resultFileName = await service.UpdateFileAsync(fileUrl, destinationPath);
-
-                        
-            string expectedDownloadedFilePath = Path.Combine(destinationPath, fakeDownloadedFileName);
-            Assert.That(File.Exists(expectedDownloadedFilePath), Is.True, "Downloaded ZIP file does not exist.");
-
-            Directory.Delete(destinationPath, true);
-            Directory.Delete(unZipFolderPath, true);
-        }*/
+        }    
       
     }
 }
