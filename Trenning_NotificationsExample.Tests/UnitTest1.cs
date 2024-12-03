9a44e3711725fb396d467dbb7bd994585a90a4c2
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-12-03 16:18:05 +0300

    Changes in project

---
+++ Trenning_NotificationsExample.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Trenning_NotificationsExample.Tests
+{
+    [TestFixture]
+    public class ExampleTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(1, 1); // Тест, который всегда проходит
+        }
+    }
+}
