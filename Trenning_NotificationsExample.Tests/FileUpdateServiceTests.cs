55ccb692e1c0f2f3703dc98c02e8b0a34eb895f9
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-12-10 17:45:21 +0300

    Tests disabled

---
+++ Trenning_NotificationsExample.Tests/FileUpdateServiceTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Trenning_NotificationsExample.Tests
+{
+    using NUnit.Framework;
+    using Trenning_NotificationsExample.Services;
+
+    [TestFixture]
+    public class FileUpdateServiceTests
+    {      
+
+        [Test]
+        public async Task UpdateFileAsync_ShouldHandleNonExistentFile()
+        {            
+            var fakeHttpHandler = new FakeHttpMessageHandler(new Dictionary<string, byte[]>());
+            var httpClient = new HttpClient(fakeHttpHandler);
+            var service = new FileUpdateService(httpClient);
+            
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                await service.UpdateFileAsync("https://example.com/missing.zip", "Destination/");
+            }, "Method threw an exception for a non-existent file.");
+        }
+
+
+    }
+}
---
+++ Trenning_NotificationsExample.Tests/Stub classes/FakePassportUpdateService.cs
@@ -0,0 +1,20 @@
+﻿using Trenning_NotificationsExample.Models;
+using Trenning_NotificationsExample.Services;
+
+namespace Trenning_NotificationsExample.Tests.Stub_classes
+{
+    public class FakePassportUpdateService : PassportUpdateService
+    {
+        public FakePassportUpdateService(string filePath) : base(filePath)
+        {
+        }
+
+        public List<PassportChange> Changes { get; } = new List<PassportChange>();
+
+        public override async Task AddChangeAsync(PassportChange change)
+        {
+            Changes.Add(change);
+            await Task.CompletedTask;
+        }
+    }
+}
